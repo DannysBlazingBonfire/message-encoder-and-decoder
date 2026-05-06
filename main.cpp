b7a75281dbd9485f6c7273a4ad41687e6255f847
Danny Gazic <dannygazic@gmail.com>
2026-05-06 20:34:10 +0200

    formatting and const correctness.

--- main.cpp
+++ main.cpp
@@ -1,122 +1,123 @@
-#include<vector>
-#include<string>
-#include<iostream>
-#include<bitset>
+#include <bitset>
 #include <cstdint>
+#include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 class Codec {
 public:
-    Codec() {};
-    ~Codec() {};
+  Codec() {};
+  ~Codec() {};
 
-    string encode(const vector<string>& strs) {
-        string encoded_str{};
-        encoded_str.reserve(sizeof(strs) + strs.size());
-        string current_str{};
-        char encrypted_char{};
+  const string encode(const vector<string> &strs) {
+    string encoded_str{};
+    encoded_str.reserve(sizeof(strs) + strs.size());
+    string current_str{};
+    char encrypted_char{};
 
-        for(size_t i{0}; i < strs.size(); i++) {
-            current_str = strs[i];
-            if(current_str.empty()) {
-                encoded_str.push_back({});
-                encoded_str.push_back(split_char);
-                continue;
-            }
+    for (size_t i{0}; i < strs.size(); i++) {
+      current_str = strs[i];
+      if (current_str.empty()) {
+        encoded_str.push_back({});
+        encoded_str.push_back(split_char);
+        continue;
+      }
 
-            // iterate characters
-            // invert bits and XOR the char byte with key.
-            for(auto _char : current_str) {
-                encrypted_char = _XOR_with_key(_invert_bits(_char));
-                encoded_str.push_back(encrypted_char);
-            }
+      // iterate characters
+      // invert bits and XOR the char byte with key.
+      for (const auto _char : current_str) {
+        encrypted_char = _XOR_with_key(_invert_bits(_char));
+        encoded_str.push_back(encrypted_char);
+      }
 
-            if(i == strs.size() - 1) { break; } // Dont add seperator at the end.
-            encoded_str.push_back(split_char);
-        }
-
-        for(size_t i{0}; i < encoded_str.size(); i++) {
-            cout << "Encoded byte: " << bitset<8>(encoded_str[i]) << endl;
-        }
-
-        return encoded_str;
+      // Dont add seperator at the end.
+      if (i == strs.size() - 1) {
+        break;
+      }
+      encoded_str.push_back(split_char);
     }
 
-    vector<string> decode(const string s) {
-        vector<string> decoded_vec{};
-        decoded_vec.reserve(sizeof(s));
-        string str_element{};
-        char decoded_char{};
-
-        for(size_t _char{0}; _char < s.length(); _char++) {
-            if(s[_char] == '\0') {
-                continue;
-            }
-
-            if(s[_char] == split_char) {
-                decoded_vec.push_back(str_element);
-                str_element.clear();
-                continue;
-            }
-
-            if(_char == s.length() - 1) {
-                decoded_char = _invert_bits(_XOR_with_key(s[_char]));
-                str_element.push_back(decoded_char);
-                decoded_vec.push_back(str_element);
-                continue;
-            }
-
-            decoded_char = _invert_bits(_XOR_with_key(s[_char]));
-            str_element.push_back(decoded_char);
-        }
-
-        for(size_t i{0}; i < decoded_vec.size(); i++) {
-            for(auto _char : decoded_vec[i]) {
-                cout << "Decoded byte: " << bitset<8>(_char) << endl;
-            }
-        }
-
-        return decoded_vec;
+    for (size_t i{0}; i < encoded_str.size(); i++) {
+      cout << "Encoded byte: " << bitset<8>(encoded_str[i]) << endl;
     }
+
+    return encoded_str;
+  }
+
+  const vector<string> decode(const string s) {
+    vector<string> decoded_vec{};
+    decoded_vec.reserve(sizeof(s));
+    string str_element{};
+    char decoded_char{};
+
+    for (size_t _char{0}; _char < s.length(); _char++) {
+      if (s[_char] == '\0') {
+        continue;
+      }
+
+      if (s[_char] == split_char) {
+        decoded_vec.push_back(str_element);
+        str_element.clear();
+        continue;
+      }
+
+      if (_char == s.length() - 1) {
+        decoded_char = _invert_bits(_XOR_with_key(s[_char]));
+        str_element.push_back(decoded_char);
+        decoded_vec.push_back(str_element);
+        continue;
+      }
+
+      decoded_char = _invert_bits(_XOR_with_key(s[_char]));
+      str_element.push_back(decoded_char);
+    }
+
+    for (size_t i{0}; i < decoded_vec.size(); i++) {
+      for (auto _char : decoded_vec[i]) {
+        cout << "Decoded byte: " << bitset<8>(_char) << endl;
+      }
+    }
+
+    return decoded_vec;
+  }
 
 private:
-    inline char _invert_bits(char c) { return (~c); }
-    inline char _XOR_with_key(char c) { return (c^encryption_key); }
+  inline char _invert_bits(char c) { return (~c); }
+  inline char _XOR_with_key(char c) { return (c ^ encryption_key); }
 
-    const uint8_t encryption_key{69};
-    const char split_char{'-'};
+  const uint8_t encryption_key{69};
+  const char split_char{'-'};
 };
 
 int main() {
-    const vector<string> msg {
-        "hello", "this", "message", "is", "encrypted."
-    };
+  const vector<string> msg{"hello", "this", "message", "is", "encrypted."};
 
-    for(auto word : msg) {
-        for(auto _char : word) {
-            cout << "Raw Bytes: " << bitset<8>(_char) << endl;
-        }
+  for (auto word : msg) {
+    for (auto _char : word) {
+      cout << "Raw Bytes: " << bitset<8>(_char) << endl;
     }
+  }
 
-    Codec codec{};
-    string encoded_message{codec.encode(msg)};
-    cout << "encoded message: " << encoded_message << endl;
-    vector<string> decoded_message {codec.decode(encoded_message)};
+  Codec codec{};
+  const string encoded_message{codec.encode(msg)};
+  cout << "encoded message: " << encoded_message << endl;
+  const vector<string> decoded_message{codec.decode(encoded_message)};
 
-    for(auto word : decoded_message) {
-        for(auto _char : word) {
-            cout << _char;
-        }
-        cout << " ";
+  for (const auto word : decoded_message) {
+    for (const auto _char : word) {
+      cout << _char;
     }
-    cout << endl;
+    cout << " ";
+  }
+  cout << endl;
 
-    if(msg == decoded_message) {
-        cout << "Input message matches output message." << endl;
-    } else {
-        cout << "Error, Message missmatch!" << endl;
-    }
+  if (msg == decoded_message) {
+    cout << "Input message matches output message." << endl;
+  } else {
+    cout << "Error, Message missmatch!" << endl;
+  }
 
-    return 0;
+  return 0;
 }
